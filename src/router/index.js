857e689233a1a3f25c319314a6ad079d7c29a0e5
doflamingo <clarkboncales115@gmail.com>
2024-11-30 07:56:54 +0800

    fix: delete registerview.vue file

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { getUserInformation, isAuthenticated } from '@/utils/supabase'
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
-import RegisterView from '@/views/auth/RegisterView.vue'
 import DashboardView from '@/views/system/DashboardView.vue'
 import ForbiddenView from '@/views/errors/ForbiddenView.vue'
 import NotFoundView from '@/views/errors/NotFoundView.vue'
@@ -18,12 +17,6 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
-      meta: { requiresAuth: false },
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: RegisterView,
       meta: { requiresAuth: false },
     },
 
