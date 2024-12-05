214026f90ffc74cbaa846e5fefd571046821aa31
RoyBayotlang <Royata243@gmail.com>
2024-12-05 17:19:53 +0800

    feat: modified the account settings section by adding edit profile log out button and change password

--- src/components/layout/ProfileHeader.vue
+++ src/components/layout/ProfileHeader.vue
@@ -1,4 +1,5 @@
 <script setup>
+// Imports
 import {
   supabase,
   formActionDefault,
@@ -8,56 +9,44 @@ import { getAvatarText } from '@/utils/helpers'
 import { useRouter } from 'vue-router'
 import { onMounted, ref } from 'vue'
 
-// Utilize pre-defined vue functions
+// Router
 const router = useRouter()
 
-// Load Variables
+// Reactive Variables
 const userData = ref({
   initials: '',
   email: '',
   fullname: '',
 })
+const formAction = ref({ ...formActionDefault })
 
-const formAction = ref({
-  ...formActionDefault,
-})
+// Functions
+const getUser = async () => {
+  const userMetadata = await getUserInformation()
+  userData.value.email = userMetadata.email
+  userData.value.fullname = `${userMetadata.firstname} ${userMetadata.lastname}`
+  userData.value.initials = getAvatarText(userData.value.fullname)
+}
 
-// Logout Functionality
 const onLogout = async () => {
-  formAction.value = { ...formActionDefault }
-
   formAction.value.formProcess = true
-
-  // Get supabase logout functionality
   const { error } = await supabase.auth.signOut()
   if (error) {
     console.error('Error during logout:', error)
     return
   }
   formAction.value.formProcess = false
-  // Redirect to homepage
   router.replace('/')
 }
 
-// Getting User Information Functionality
-const getUser = async () => {
-  // Retrieve User Information
-  const userMetadata = await getUserInformation()
-
-  // Assign the retrieved informations
-  userData.value.email = userMetadata.email
-  userData.value.fullname = userMetadata.firstname + ' ' + userMetadata.lastname
-  userData.value.initials = getAvatarText(userData.value.fullname)
-}
-
-// Load Functions during component rendering
+// Lifecycle Hook
 onMounted(() => {
   getUser()
 })
 </script>
 
 <template>
-  <v-menu min-width="200px" rounded>
+  <v-menu min-width="350px" rounded>
     <template #activator="{ props }">
       <v-btn icon v-bind="props">
         <v-avatar color="deep-orange-lighten-1" size="large">
@@ -65,28 +54,47 @@ onMounted(() => {
         </v-avatar>
       </v-btn>
     </template>
-    <v-card class="mt-1">
+    <v-card class="mt-2" elevation="3">
       <v-card-text>
-        <v-list>
-          <v-list-item :subtitle="userData.email" :title="userData.fullname">
-            <template #prepend>
-              <v-avatar color="deep-orange-lighten-1" size="large">
-                <span class="text-h5">{{ userData.initials }}</span>
-              </v-avatar>
-            </template>
-          </v-list-item>
-        </v-list>
+        <v-row align="center">
+          <!-- Profile Picture -->
+          <v-col cols="4" class="text-center">
+            <v-avatar color="deep-orange-lighten-1" size="x-large">
+              <span class="text-h5">{{ userData.initials }}</span>
+            </v-avatar>
+          </v-col>
+          <!-- Profile Info -->
+          <v-col cols="8">
+            <v-btn
+              prepend-icon="mdi-pencil"
+              variant="outlined"
+              color="primary"
+              class="mb-3"
+              block
+            >
+              Edit Profile
+            </v-btn>
+            <div class="profile-details">
+              <h3 class="mb-1">{{ userData.fullname }}</h3>
+              <p class="text-secondary">{{ userData.email }}</p>
+            </div>
+          </v-col>
+        </v-row>
         <v-divider class="my-3"></v-divider>
-        <v-btn prepend-icon="mdi-wrench" variant="plain">
-          Account Settings
+        <!-- Change Password Button -->
+        <v-btn prepend-icon="mdi-lock" variant="outlined" class="mb-3" block>
+          Change Password
         </v-btn>
         <v-divider class="my-3"></v-divider>
+        <!-- Logout Button -->
         <v-btn
           prepend-icon="mdi-logout"
-          variant="plain"
+          color="error"
+          variant="outlined"
           @click="onLogout"
           :loading="formAction.formProcess"
           :disabled="formAction.formProcess"
+          block
         >
           Logout
         </v-btn>
@@ -94,3 +102,21 @@ onMounted(() => {
     </v-card>
   </v-menu>
 </template>
+
+<style scoped>
+.profile-details h3 {
+  margin: 0;
+  font-size: 1.25rem;
+  font-weight: bold;
+}
+
+.profile-details p {
+  margin: 0;
+  font-size: 0.9rem;
+  color: gray;
+}
+
+.v-btn {
+  text-transform: none;
+}
+</style>
