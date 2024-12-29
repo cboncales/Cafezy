b961c337226bbcdadf657427f6368ee787821ab2
cboncales <clarkboncales115@gmail.com>
2024-12-29 12:34:53 +0800

    feat: added table pagination and tablesearch in supabase.js
    Co-Authored-By: RoyBayotlang <179607343+RoyBayotlang@users.noreply.github.com>
    Co-Authored-By: stanleyapal <180125859+stanleyapal@users.noreply.github.com>

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -26,4 +26,30 @@ export const isAuthenticated = async () => {
   return !!data.session
 }
 
+// 👉 Table Pagination
+export const tablePagination = (
+  { page, itemsPerPage, sortBy },
+  defaultColumn = 'id',
+  isAscending = true,
+) => {
+  const [column, order] = sortBy[0]
+    ? [sortBy[0].key, sortBy[0].order === 'asc']
+    : [defaultColumn, isAscending]
 
+  if (itemsPerPage === -1) {
+    const rangeStart = 0
+    const rangeEnd = 999999999999999
+
+    return { rangeStart, rangeEnd, column, order }
+  }
+
+  const rangeStart = (page - 1) * itemsPerPage
+  const rangeEnd = rangeStart + itemsPerPage - 1
+
+  return { rangeStart, rangeEnd, column, order }
+}
+
+// 👉 Handle Search if null turn to empty string
+export const tableSearch = search => {
+  return (search ||= '')
+}
